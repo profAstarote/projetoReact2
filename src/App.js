39df685144c3fd8d496892d7303328982cb725cd
Astarote <astarote@gmail.com>
2026-04-21 09:19:13 -0300

    corrigido o packege.json home e script

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          prof° Astarote - projeto AvançaTech
         </p>
         <a
           className="App-link"
